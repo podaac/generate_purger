aeab84a0b630fa4d671ad79ed5e1faca4c7ebf16
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-03-28 14:21:16 +0000

    Enable EventBridge schedule for 2300

--- purger-lambda.tf
+++ purger-lambda.tf
@@ -135,60 +135,60 @@ resource "aws_iam_policy" "aws_lambda_execution_policy" {
   })
 }
 
-# # EventBridge schedule
-# resource "aws_scheduler_schedule" "aws_schedule_purger" {
-#   name       = "${var.prefix}-purger"
-#   group_name = "default"
-#   flexible_time_window {
-#     mode = "OFF"
-#   }
-#   schedule_expression = "cron(00 23 * * ? *)"
-#   target {
-#     arn      = aws_lambda_function.aws_lambda_purger.arn
-#     role_arn = aws_iam_role.aws_eventbridge_purger_execution_role.arn
-#     input = jsonencode({
-#       "prefix" : "${var.prefix}"
-#     })
-#   }
-# }
+# EventBridge schedule
+resource "aws_scheduler_schedule" "aws_schedule_purger" {
+  name       = "${var.prefix}-purger"
+  group_name = "default"
+  flexible_time_window {
+    mode = "OFF"
+  }
+  schedule_expression = "cron(00 23 * * ? *)"
+  target {
+    arn      = aws_lambda_function.aws_lambda_purger.arn
+    role_arn = aws_iam_role.aws_eventbridge_purger_execution_role.arn
+    input = jsonencode({
+      "prefix" : "${var.prefix}"
+    })
+  }
+}
 
-# # EventBridge execution role and policy
-# resource "aws_iam_role" "aws_eventbridge_purger_execution_role" {
-#   name = "${var.prefix}-eventbridge-purger-execution-role"
-#   assume_role_policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Effect" : "Allow",
-#         "Principal" : {
-#           "Service" : "scheduler.amazonaws.com"
-#         },
-#         "Action" : "sts:AssumeRole"
-#       }
-#     ]
-#   })
-#   permissions_boundary = "arn:aws:iam::${local.account_id}:policy/NGAPShRoleBoundary"
-# }
+# EventBridge execution role and policy
+resource "aws_iam_role" "aws_eventbridge_purger_execution_role" {
+  name = "${var.prefix}-eventbridge-purger-execution-role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "scheduler.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+  permissions_boundary = "arn:aws:iam::${local.account_id}:policy/NGAPShRoleBoundary"
+}
 
-# resource "aws_iam_role_policy_attachment" "aws_eventbridge_purger_execution_role_policy_attach" {
-#   role       = aws_iam_role.aws_eventbridge_purger_execution_role.name
-#   policy_arn = aws_iam_policy.aws_eventbridge_purger_execution_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "aws_eventbridge_purger_execution_role_policy_attach" {
+  role       = aws_iam_role.aws_eventbridge_purger_execution_role.name
+  policy_arn = aws_iam_policy.aws_eventbridge_purger_execution_policy.arn
+}
 
-# resource "aws_iam_policy" "aws_eventbridge_purger_execution_policy" {
-#   name        = "${var.prefix}-eventbridge-purger-execution-policy"
-#   description = "Allow EventBridge to invoke a Lambda function."
-#   policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Sid" : "AllowInvokeLambda",
-#         "Effect" : "Allow",
-#         "Action" : [
-#           "lambda:InvokeFunction"
-#         ],
-#         "Resource" : "${aws_lambda_function.aws_lambda_purger.arn}"
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_policy" "aws_eventbridge_purger_execution_policy" {
+  name        = "${var.prefix}-eventbridge-purger-execution-policy"
+  description = "Allow EventBridge to invoke a Lambda function."
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowInvokeLambda",
+        "Effect" : "Allow",
+        "Action" : [
+          "lambda:InvokeFunction"
+        ],
+        "Resource" : "${aws_lambda_function.aws_lambda_purger.arn}"
+      }
+    ]
+  })
+}
