460c7cf17a3a4a95f2eea5f9488564199df1074a
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-07-25 14:27:52 +0000

    Install netcdf4 library in purger zip package

--- deploy/deploy-lambda.sh
+++ deploy/deploy-lambda.sh
@@ -11,7 +11,7 @@ APP_NAME=$1
 ROOT_PATH="$PWD"
 
 # Install dependencies
-pip install --target $ROOT_PATH/package typing-extensions requests~=2.29.0 fsspec~=2023.3.0 s3fs~=2023.3.0
+pip install --target $ROOT_PATH/package typing-extensions requests~=2.29.0 fsspec~=2023.3.0 s3fs~=2023.3.0 netCDF4~=1.6.4
 
 # Zip dependencies
 cd package/
